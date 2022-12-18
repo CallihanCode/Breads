5d268eb9fe30fb890210a1c8cc400441b4b9f04b
Tim Callihan <callihanphoto@gmail.com>
2022-12-18 08:37:40 -0500

    Adding UTD version as of 12/18

--- controllers/bread.js
+++ controllers/bread.js
@@ -1,13 +1,86 @@
 const router = require('express').Router()
 const Bread = require('../models/bread')
+const Baker = require('../models/baker')
+const seedData = require('../models/seedData')
 
-router.get('/', (req, res) => {
-    res.send(Bread)
+router.get('/', async (req, res) => {
+    const bread = await Bread.find()
+    const bakers = await Baker.find()
+    res.render('index', {
+        breads: bread,
+        bakers
+    })
+
 })
-// query paramater
-router.get('/:index', (req, res) => {
-    const { index } = req.params
-    res.send(Bread[index])
+
+// Get:create new bread page
+router.get('/new', async (req, res) => {
+    const bakers = await Baker.find()
+    res.render('new', {
+        bakers
+    })
+})
+
+//Get: Edit bread page
+router.get('/:id/edit', async (req, res) => {
+    const { id } = req.params
+    const bread = await Bread.findById(id)
+    const bakers = await Baker.find()
+    res.render('edit', {
+        bread, bakers
+    })
+} )
+
+//query parameter
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const bread = await Bread.findById(id).populate('baker')
+    res.render('show', {
+        bread
+    })
+})
+
+router.get('/data/seed', async (req, res) => {
+    await Bread.insertMany(seedData)
+    res.redirect('/breads')
+})
+
+router.get('/data/delete', async (req,res) => {
+    await Bread.deleteMany()
+    res.redirect('/breads')
+})
+
+router.post('/', async (req, res) => {
+    const { hasGluten, image } = req.body
+    if(!image) req.body.image = undefined
+    if (hasGluten === 'on') {
+        req.body.hasGluten = true;
+    } else {
+        req.body.hasGluten = false;
+    }
+    
+    await Bread.create(req.body)
+    res.redirect('/breads')
+})
+
+router.put('/:id', async (req, res) => {
+    const { id } = req.params
+    const { image, hasGluten } = req.body
+    if (!image) req.body.image = 'https://suebeehomemaker.com/wp-content/uploads/2021/10/sliced-french-bread.jpg'
+    if (hasGluten === 'on') {
+        req.body.hasGluten = true;
+    } else {
+        req.body.hasGluten = false;
+    }
+    await Bread.findByIdAndUpdate(id, req.body)
+    res.redirect(`/breads/${id}`)
+})
+
+// DELETE a bread
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params
+    await Bread.findByIdAndDelete(id)
+    res.status(303).redirect('/breads')
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express')
+const methodOverride = require('method-override')
+const mongoose = require ('mongoose')
 require('dotenv').config()
 
 const breadRoutes = require('./controllers/bread') 
-
+const bakerRoutes = require('./controllers/baker')
 const app = express()
 
-app.use('/breads', breadRoutes)
+// MIDDLEWARE
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true}))
+app.use(methodOverride('_method'))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
+//Routes
+app.use('/breads', breadRoutes)
+app.use('/baker', bakerRoutes)
 app.get('/', (req, res) => {
     res.send('<h1>hello</h1>')
 })
+
+mongoose.set('strictQuery', true)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
 
 const PORT = process.env.PORT || 8080
 
